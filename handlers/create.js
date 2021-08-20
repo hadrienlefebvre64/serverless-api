c675b1865bf88b6f4c2920f69236109a84a662ee
Hadrien <hadrien@armis.tech>
2021-08-20 12:11:01 +0200

    feat: Connect to Hubspot API and send event

--- handlers/create.js
+++ handlers/create.js
@@ -3,13 +3,42 @@
 module.exports = async (data) => {
     const params = {
         Ticket: {
-            name: data.name,
-            quantity: data.quantity,
-            addedAt: Date.now(),
+            hs_pipeline: data.hs_pipeline,
+            hs_pipeline_stage: data.hs_pipeline_stage,
+            hs_ticket_priority: data.hs_ticket_priority,
+            hubspot_owner_id: data.hubspot_owner_id,
+            subject: data.subject,
         }
     };
-    // return dynamoDb.put(params).promise()
-    //     .then(result => params.Item)
-    return console.log('HHHHHHHHHHHHHHHH', params);
-    // Here call the Hubspot create API
+    console.log('Params.ticket : ' + params.Ticket);
+    return createHubspotTicket(params.Ticket).then((res) =>
+        JSON.stringify(res)
+    );
 };
+
+// Call Hubspot API to create a new ticket
+async function createHubspotTicket(ticket) {
+
+    const hubspot = require('@hubspot/api-client');
+
+    const hubspotClient = new hubspot.Client({"apiKey": "eu1-4686-1b46-46df-b390-e4bf38cf3efa"});
+
+    const properties = {
+        "hs_pipeline": ticket.hs_pipeline,
+        "hs_pipeline_stage": ticket.hs_pipeline_stage,
+        "hs_ticket_priority": ticket.hs_ticket_priority,
+        "hubspot_owner_id": ticket.hubspot_owner_id,
+        "subject": ticket.subject
+    };
+    const simplePublicObjectInput = {properties};
+
+    try {
+        const apiResponse = await hubspotClient.crm.tickets.basicApi.create(simplePublicObjectInput);
+        console.log(JSON.stringify(apiResponse.body, null, 2));
+    } catch (e) {
+        e.message === 'HTTP request failed'
+            ? console.error(JSON.stringify(e.response, null, 2))
+            : console.error(e)
+    }
+}
+
